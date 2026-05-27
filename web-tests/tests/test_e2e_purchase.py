24f272fc73c9b9d9138d0743fb176b3de9d8abda
Eike Develop <105676044+eikedaniell@users.noreply.github.com>
2026-05-27 17:08:30 -0300

    fix: corrige senha do usuario no teste e2e

--- web-tests/tests/test_e2e_purchase.py
+++ web-tests/tests/test_e2e_purchase.py
@@ -1,2 +1,23 @@
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+from pages.cart_page import CartPage
+from pages.checkout_page import CheckoutPage
+
 def test_end_to_end_purchase(driver):
-    login_page.login("standard_user", "password")  
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page = InventoryPage(driver)
+    inventory_page.add_item_to_cart()
+    inventory_page.go_to_cart()
+
+    cart_page = CartPage(driver)
+    cart_page.proceed_to_checkout()
+
+    checkout_page = CheckoutPage(driver)
+    checkout_page.fill_info("Joao", "Silva", "12345")
+    checkout_page.finish_order()
+
+    msg = checkout_page.get_success_message()
+    assert "Thank you for your order" in msg
