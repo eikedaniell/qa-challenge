0dd1b4a3930cf4b031d74db6f746a14daef5485d
Eike Develop <105676044+eikedaniell@users.noreply.github.com>
2026-05-27 17:07:58 -0300

    fix: corrige seletor generico por ID correto no login page

--- web-tests/pages/login_page.py
+++ web-tests/pages/login_page.py
@@ -1,3 +1,18 @@
+from selenium.webdriver.common.by import By
+
 class LoginPage:
+    URL = "https://www.saucedemo.com/"
+    
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.ID, "user-name")
+        self.password_input = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
+
+    def open(self):
+        self.driver.get(self.URL)
+
     def login(self, username, password):
-        self.driver.find_element(By.XPATH, "//input[@type='text']").send_keys(username)
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
